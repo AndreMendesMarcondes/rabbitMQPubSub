8110c7c1b528e93f94615ba893663e3caa01524f
André Mendes <andre.marcondes@betalearning.com.br>
2021-08-30 22:46:58 -0300

    initiatin project, create post endpoint

---
+++ RMQPS.Pub/Controllers/PublisherController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RMQPS.Pub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublisherController : ControllerBase
+    {
+
+        [HttpPost]
+        public IActionResult Post(string message)
+        {
+            return StatusCode(201);
+        }
+    }
+}
